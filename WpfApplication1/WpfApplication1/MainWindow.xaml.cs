eeba931505a95ae9c315415d1aa6008f4986910f
lproductions <l.productionsuk@gmail.com>
2017-08-07 20:09:46 +0100

    cleaned up the code. Added functionality to filter results.

--- WpfApplication1/WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -13,45 +13,48 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Net;
-using System.Linq;
+
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
 namespace WpfApplication1 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    /// 
-
+    
     // using Json.net 9.0.1 because newer versions do not work iwth VS 2012/2013 ----- https://stackoverflow.com/questions/44532170/newtonsoft-json-already-has-a-dependency-defined-for-microsoft-csharp
 
     public partial class MainWindow : Window {
+
+        // Filters for memes to find.
+        public string SearchSubReddit = "prequelmemes"; // only prequel memes at the moment but can expand to more later on.
+        public string SearchTitle = "Obi"; //defaults to Obi memes
+        public string SearchSort = "new"; //defaults to the newest memes
+        public int SearchAmount = 5; // not used yet but will be used once the Application can display multiple results. Defaults to 5 so it will get the five newest and display the newest one out of them.
+
         public MainWindow() {
             InitializeComponent();
+
+
+            
+            Meme.Source = StartSearchBitmapImage() ;
+
+        }
+
+
+
+        public BitmapImage StartSearchBitmapImage(){
             WebClient client = new WebClient();
 
-           
+            // Get Json from the reddit api.
+            string result = client.DownloadString(String.Format("https://www.reddit.com/r/{0}/search.json?q={1}&sort={2}&restrict_sr=on&limit={3}", SearchSubReddit, SearchTitle, SearchSort, SearchAmount));
+            var jsonresult = JObject.Parse(result);
 
 
-
-            string result = client.DownloadString("https://www.reddit.com/r/prequelmemes/search.json?q=obi&sort=new&restrict_sr=on&limit=5");
-            var jsonresult =  JObject.Parse(result);
+            // Get the source URL from the Json.
+            var jsonurl = jsonresult["data"]["children"][0]["data"]["url"];
+            Console.WriteLine(jsonurl.ToString());
             
 
-
-            var jsonurl = jsonresult["data"]["children"][0]["data"]["url"];//[0]["url"];
-            Console.WriteLine(jsonurl.ToString());
-            //Title.Text = jsonurl.ToString();  --- was for testing
-
-
-
-            Uri uristring = new Uri(jsonurl.ToString() );
-            var image = new BitmapImage(uristring );
-            Meme.Source = image ;
-                
-                
-
-           
-           // Console.WriteLine(response.ContentType);
+            // Get the image from the URL.
+            Uri uristring = new Uri(jsonurl.ToString());
+            return new BitmapImage(uristring);
 
         }
     }
